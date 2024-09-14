6a72483843a5bc506283be1b9278d0ec1b9270dd
jojczykp <pawel.jojczyk@gmail.com>
2024-09-14 19:21:36 +0200

    Big bang!

---
+++ src/main/kotlin/org/alterbit/Application.kt
@@ -0,0 +1,12 @@
+package org.alterbit
+
+import io.ktor.server.application.*
+import org.alterbit.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
